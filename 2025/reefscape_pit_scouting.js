7b8527bc776e8a410b672a0ee658ab057d7e7fa1
Trey3160 <treyton.robotics@gmail.com>
2025-04-04 13:30:27 -0500

    Update reefscape_pit_scouting.js

--- 2025/reefscape_pit_scouting.js
+++ 2025/reefscape_pit_scouting.js
@@ -9,7 +9,7 @@ var config_data = `
       "code": "t",
       "type": "number"
     },
-    { "name": "Width",
+    { "name": "Dimentions",
       "code": "wid",
       "type": "number",
       "defaultValue": "0"
@@ -37,19 +37,6 @@ var config_data = `
       "size": 20,
       "maxSize": 50
     },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
     { "name": "Drivetrain Motor",
       "code": "mot",
       "type": "radio",
@@ -60,10 +47,6 @@ var config_data = `
         "x": "Other<br>"
       },
       "defaultValue":"x"
-    },
-    { "name": "# of Batteries",
-      "code": "nob",
-      "type": "number"
     },
     { "name": "Floor pickup Coral",
       "code": "fpu",
