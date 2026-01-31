fc417ae5ae78bbdb8fb81eb281715f5ce0d8f923
Trey3160 <treyton.robotics@gmail.com>
2026-01-31 11:54:51 -0600

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026okok",
+      "defaultValue": "{ "2026ilch",
       "required": "true"
       },
     { "name": "Match Level",
