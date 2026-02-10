31f98e10ded52b1f5c35e3c48b1900a41fee1683
Trey3160 <treyton.robotics@gmail.com>
2026-02-09 18:07:52 -0600

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -3,26 +3,22 @@ var config_data = `
   "dataFormat": "tsv",
   "title": "Scouting PASS 2026",
   "page_title": "REBUILT",
-  "checkboxAs": 10,
-
+  "checkboxAs": "10",
   "prematch": [
-    {
-      "name": "Scouter Initials",
+    { "name": "Scouter Initials",
       "code": "s",
       "type": "scouter",
       "size": 5,
       "maxSize": 5,
-      "required": true
+      "required": "true"
     },
-    {
-  "name": "Event",
-  "code": "e",
-  "type": "event",
-  "defaultValue": "2026ilpe",
-  "required": true
-},
-    {
-      "name": "Match Level",
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2026ilch",
+      "required": "true"
+    },
+    { "name": "Match Level",
       "code": "l",
       "type": "level",
       "choices": {
@@ -31,18 +27,16 @@ var config_data = `
         "f": "Finals"
       },
       "defaultValue": "qm",
-      "required": true
+      "required": "true"
     },
-    {
-      "name": "Match #",
+    { "name": "Match #",
       "code": "m",
       "type": "match",
       "min": 1,
       "max": 150,
-      "required": true
+      "required": "true"
     },
-    {
-      "name": "Robot",
+    { "name": "Robot",
       "code": "r",
       "type": "robot",
       "choices": {
@@ -53,17 +47,15 @@ var config_data = `
         "r3": "Red-3",
         "b3": "Blue-3"
       },
-      "required": true
+      "required": "true"
     },
-    {
-      "name": "Team #",
+    { "name": "Team #",
       "code": "t",
       "type": "team",
       "min": 1,
       "max": 99999
     },
-    {
-      "name": "Auto Start Location",
+    { "name": "Auto Start Location",
       "code": "as",
       "type": "clickable_image",
       "filename": "2026/half_field.png",
@@ -73,58 +65,99 @@ var config_data = `
       "shape": "circle 5 black red true"
     }
   ],
-
   "auton": [
-    { "name": "Scored Fuel", "code": "sf", "type": "bool" },
-    { "name": "Went to Middle", "code": "wtm", "type": "bool" },
-    {
-      "name": "Climb (L1)",
+    { "name": "Auto Shooting Location",
+      "code": "asl",
+      "type": "clickable_image",
+      "filename": "2026/half_field.png",
+      "dimensions": "7 10",
+      "allowableResponses": "1 2 3 4 8 9 10 11 15 16 17 18 22 23 24 25 29 30 31 32 36 37 38 39 43 44 45 46 50 51 52 53 57 58 59 60 64 65 66 67",
+      "expectedMax": 5,
+      "shape": "circle 5 black red true"
+    },
+    { "name": "Fuel Scored",
+      "code": "afs",
+      "expectedMax": 32,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+    },
+    { "name": "Pass from Neutral Zone",
+      "code": "apn",
+      "expectedMax": 60,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+    },
+    { "name": "Climb (L1)",
       "code": "ac",
       "type": "radio",
       "choices": {
-        "Climbed": "Climbed<br>",
-        "Attempted": "Attempted<br>",
-        "Not Attempted": "Not Attempted"
+        "c": "Climbed<br>",
+        "a": "Attempted<br>",
+        "x": "Not Attempted"
       },
-      "defaultValue": "Not Attempted"
+      "defaultValue": "x"
     },
-    { "name": "Pickup from Depot", "code": "afd", "type": "bool" },
-    { "name": "Pickup from Outpost", "code": "afo", "type": "bool" }
+    { "name": "Pickup from Depot",
+      "code": "afd",
+      "type": "bool"
+    },
+    { "name": "Pickup from Outpost",
+      "code": "afo",
+      "type": "bool"
+    },
+    { "name": "Pickup from Neutral Zone",
+      "code": "aff",
+      "type": "bool"
+    }
   ],
-
   "teleop": [
-    {
-      "name": "Offensive Shift Strategy",
-      "code": "oss",
-      "type": "radio",
-      "choices": {
-        "1": "Best Scorrer<br>",
-        "2": "2nd Best Scorrer<br>",
-        "3": "3rd Best Scorrer<br>",
-        "0": "Defense"
-      },
-      "defaultValue": "0"
+    { "name": "Shooting Locations",
+      "code": "tsl",
+      "type": "clickable_image",
+      "filename": "2026/half_field.png",
+      "dimensions": "7 10",
+      "allowableResponses": "1 2 3 4 8 9 10 11 15 16 17 18 22 23 24 25 29 30 31 32 36 37 38 39 43 44 45 46 50 51 52 53 57 58 59 60 64 65 66 67",
+      "expectedMax": 25,
+      "shape": "circle 5 black red true"
     },
-    {
-      "name": "Defensive Shift Strategy",
-      "code": "dss",
-      "type": "radio",
-      "choices": {
-        "f": "Feeder<br>",
-        "n": "Defense Nutural Zone<br>",
-        "z": "Defense Alliance Zone"
-      },
-      "defaultValue": "f"
+    { "name": "Fuel Scored",
+      "code": "tfs",
+      "expectedMax": 150,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
     },
-    { "name": "Under Trench", "code": "ut", "type": "bool" },
-    { "name": "Over Bump", "code": "ob", "type": "bool" },
-    { "name": "Pickedup from Human Player", "code": "pfh", "type": "bool" },
-    { "name": "Pickup from Floor", "code": "pff", "type": "bool" }
+    { "name": "Pass from Neutral Zone",
+      "code": "pnz",
+      "expectedMax": 250,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+    },
+    { "name": "Pass from Opp Alliance Zone",
+      "code": "poa",
+      "expectedMax": 250,
+      "altInc1": 10,
+      "altInc2": 5,
+      "type": "counter"
+    },
+    { "name": "Pickup from Depot",
+      "code": "tfd",
+      "type": "bool"
+    },
+    { "name": "Pickup from Outpost",
+      "code": "tfo",
+      "type": "bool"
+    },
+    { "name": "Pickup from Floor",
+      "code": "tff",
+      "type": "bool"
+    }
   ],
-
   "endgame": [
-    {
-      "name": "Climb",
+    { "name": "Climb",
       "code": "tc",
       "type": "radio",
       "choices": {
@@ -137,10 +170,8 @@ var config_data = `
       "defaultValue": "x"
     }
   ],
-
   "postmatch": [
-    {
-      "name": "Driver Skill",
+    { "name": "Driver Skill",
       "code": "ds",
       "type": "radio",
       "choices": {
@@ -151,8 +182,7 @@ var config_data = `
       },
       "defaultValue": "x"
     },
-    {
-      "name": "Defense Rating",
+    { "name": "Defense Rating",
       "code": "dr",
       "type": "radio",
       "choices": {
@@ -164,8 +194,7 @@ var config_data = `
       },
       "defaultValue": "x"
     },
-    {
-      "name": "Speed Rating",
+    { "name": "Speed Rating",
       "code": "sr",
       "type": "radio",
       "choices": {
@@ -175,23 +204,49 @@ var config_data = `
         "4": "4<br>",
         "5": "5 (fast)"
       },
-      "defaultValue": "3"
+      "defaultValue":"3"
     },
-    { "name": "Died/Immobilized", "code": "die", "type": "bool" },
-    { "name": "Tippy<br>(almost tipped over)", "code": "tip", "type": "bool" },
-    {
-      "name": "Make good<br>alliance partner?",
+    { "name": "Crossed Bump",
+      "code": "bmp",
+      "type": "bool"
+    },
+    { "name": "Crossed Trench",
+      "code": "tre",
+      "type": "bool"
+    },
+    { "name": "Died/Immobilized",
+      "code": "die",
+      "type": "bool"
+    },
+    { "name": "Tippy<br>(almost tipped over)",
+      "code": "tip",
+      "type": "bool"
+    },
+    { "name": "Make good<br>alliance partner?",
       "tooltip": "Would you want this robot on your alliance in eliminations?",
       "code": "all",
       "type": "bool"
     },
-    { "name": "Was Defended", "code": "def", "type": "bool" },
-    {
-      "name": "Comments",
+    { "name": "Was Defended",
+      "code": "def",
+      "type": "bool"
+    },
+    { "name": "Excessive Penalties",
+      "code": "pen",
+      "type": "bool"
+    },
+    { "name": "Fuel Percentage",
+      "tooltip": "What percentage of the total fuel for this alliance did this robot score?",
+      "code": "pct",
+      "type": "number",
+      "min": 0,
+      "max": 100
+    },
+    { "name": "Comments",
       "code": "co",
       "type": "text",
       "size": 15,
       "maxSize": 55
     }
   ]
-}';
+}`;
