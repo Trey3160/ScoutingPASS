170c923bab3ea56a1e752dfaeed44a74f4bd9cfb
Trey3160 <treyton.robotics@gmail.com>
2026-02-10 18:40:41 -0600

    Update rebuilt_pit_scouting.js

--- 2026/rebuilt_pit_scouting.js
+++ 2026/rebuilt_pit_scouting.js
@@ -31,40 +31,6 @@ var config_data = `
       },
       "defaultValue": "o"
     },
-    { "name": "Other Drivetrain",
-      "code": "odt",
-      "type": "text",
-      "size": 20,
-      "maxSize": 50
-    },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "Drivetrain Motor",
-      "code": "mot",
-      "type": "radio",
-      "choices": {
-        "n": "Neo<br>",
-        "f": "Falcon<br>",
-        "c": "CIM<br>",
-        "x": "Other<br>"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "# of Batteries",
-      "code": "nob",
-      "type": "number"
-    },
     { "name": "Floor pickup Fuel",
       "code": "fpu",
       "type": "bool"
@@ -77,18 +43,12 @@ var config_data = `
       "code": "opu",
       "type": "bool"
     },
-    { "name": "Autos",
-      "code": "aut",
+    { "name": "What do they belive makes their robot stand out",
+      "code": "tco",
       "type": "text",
       "size": 20,
       "maxSize": 250
-    },
-    { "name": "Scouting Method /<br>Program (ScoutingPASS?)",
-      "code": "sct",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
+    }
     { "name": "Comments",
       "code": "co",
       "type": "text",
